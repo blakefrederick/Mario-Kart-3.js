cb947f5c9ebaf3e101ff451e697cbb8361c1b2d5
Blake Frederick <bf@six12creative.com>
2024-02-28 22:03:43 -0800

    Add the most basic music component imaginable

---
+++ src/Music.jsx
@@ -0,0 +1,5 @@
+export const Music = () => {
+  const audio = new Audio("https://dl.vgmdownloads.com/soundtracks/mario-kart-wii/jahdscxtwb/89.%20DS%20Delfino%20Square.mp3")
+  audio.play()
+  return null
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App'
 import './index.css'
 import { HUD } from './HUD'
 import { Landing } from './Landing'
+import { Music } from './Music'
 import { useStore } from './components/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <HUD />
+    <Music />
     <Landing />
   </React.StrictMode>,
 )
