053b98d7de7c76adbd904b109f675a2c467bda39
Blake Frederick <bf@six12creative.com>
2024-02-28 22:31:20 -0800

    Add a volume slider

--- src/Music.jsx
+++ src/Music.jsx
@@ -1,20 +1,41 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useStore } from './components/store'
 
 export const Music = () => {
   const { gameStarted } = useStore()
+  const [volume, setVolume] = useState(0.75)
+  const [audio] = useState(() => new Audio("https://dl.vgmdownloads.com/soundtracks/mario-kart-wii/jahdscxtwb/89.%20DS%20Delfino%20Square.mp3")) // one only
 
   useEffect(() => {
-    const audio = new Audio("https://dl.vgmdownloads.com/soundtracks/mario-kart-wii/jahdscxtwb/89.%20DS%20Delfino%20Square.mp3")
     audio.loop = true
 
     if (gameStarted) {
       audio.play()
+    } else {
+      audio.pause()
+      audio.currentTime = 0
     }
+
     return () => {
       audio.pause()
     }
-  }, [gameStarted])
+  }, [gameStarted, audio])
 
-  return null
+  useEffect(() => {
+    audio.volume = volume
+  }, [volume, audio])
+
+  return (
+    <div style={{ position: 'fixed', bottom: 10, left: 0, right: 0, backgroundColor: 'rgba(0,0,0,0.5)', width: '10%'}}>
+      <input
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        value={volume}
+        onChange={(e) => setVolume(e.target.value)}
+        style={{ width: '100%' }}
+      />
+    </div>
+  )
 }
