32b86b8f0d52fa8af147b392335dd4822832b6e3
Blake Frederick <bf@six12creative.com>
2024-02-28 22:11:19 -0800

    Grab the `gameStarted` context var and use it to init the music when the level starts

--- src/Music.jsx
+++ src/Music.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react'
+import { useStore } from './components/store'
+
 export const Music = () => {
-  const audio = new Audio("https://dl.vgmdownloads.com/soundtracks/mario-kart-wii/jahdscxtwb/89.%20DS%20Delfino%20Square.mp3")
-  audio.play()
+  const { gameStarted } = useStore()
+
+  useEffect(() => {
+    const audio = new Audio("https://dl.vgmdownloads.com/soundtracks/mario-kart-wii/jahdscxtwb/89.%20DS%20Delfino%20Square.mp3")
+    if (gameStarted) {
+      audio.play()
+    }
+  }, [gameStarted])
+
   return null
-};
+}
+    
